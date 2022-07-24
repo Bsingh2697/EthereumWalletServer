669659ac62f32d89d1f58de1a137866a0ada5e91
Bharat Singh <bharatsingh@Bharats-MacBook-Pro.local>
2022-07-24 01:15:58 +0100

    Push On AWS

--- dist/app.js
+++ dist/app.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 app.get('/', (req, res, next) => {
-    res.send('Hello WELCOME');
+    res.send('Hello WELCOME ENV 2');
 });
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log("Server running"));
