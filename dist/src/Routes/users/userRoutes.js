5841a0dc92441974fc0d649f4b986c1d3e0481f0
Bharat Singh <bharatsingh@aescoder.local>
2022-08-17 16:59:23 +0100

    some changes hoping it's fixed

--- dist/src/Routes/users/userRoutes.js
+++ dist/src/Routes/users/userRoutes.js
@@ -1,30 +1,35 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const controller = require('../../Controller/userController.ts');
-const jwt = require('jsonwebtoken');
+const controller = require("../../Controller/userController.js");
+const jwt = require("jsonwebtoken");
 const router = express_1.default.Router();
-const auth = require('../../Middlewares/auth');
-const User = require('../../models/UserModel/User');
-const { registerValidation, loginValidation } = require('../../Validation/validation');
+const auth = require("../../Middlewares/auth");
+const User = require("../../models/UserModel/User");
+const {
+  registerValidation,
+  loginValidation,
+} = require("../../Validation/validation");
 /*
-** Order of routes matter
-** If we put "/key" route under "/:user_id" then
-** we won't be able to access "/key" route
-*/
+ ** Order of routes matter
+ ** If we put "/key" route under "/:user_id" then
+ ** we won't be able to access "/key" route
+ */
 // ********************** Find All Users **********************
-router.get('/', controller.fetchAllUsers);
+router.get("/", controller.fetchAllUsers);
 // ********************** Find key by ID **********************
-router.get('/key', auth, controller.fetchUserPrivateKey);
+router.get("/key", auth, controller.fetchUserPrivateKey);
 // ********************** Find user by ID **********************
-router.get('/:user_id', auth, controller.fetchUserDetails);
+router.get("/:user_id", auth, controller.fetchUserDetails);
 // ********************** Remove user by ID **********************
-router.delete('/:user_id', auth, controller.deleteUser);
+router.delete("/:user_id", auth, controller.deleteUser);
 // ********************** SIGNUP - ADD NEW USER **********************
-router.post('/signup', controller.signup);
+router.post("/signup", controller.signup);
 // ********************** LOGIN - USER **********************
-router.post('/signin', controller.signin);
+router.post("/signin", controller.signin);
 module.exports = router;
