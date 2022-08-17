0789748afbc2104d6280530671f4fd356d7e06fb
Bharat Singh <bharatsingh@aescoder.local>
2022-08-17 12:15:29 +0100

    User controller fix

--- dist/server.js
+++ dist/server.js
@@ -1,21 +1,10 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 // ********************** IMPORTS **********************
 const express_1 = __importDefault(require("express"));
-const DatabaseConnection_1 = require("./src/DatabaseConnection");
-const UrlConstants_1 = require("./src/utils/constants/UrlConstants");
 // ********************** Module Imports **********************
 const cors = require('cors');
 // ********************** IMPORT ROUTES **********************
@@ -23,18 +12,18 @@ const usersRoute = require('./src/Routes').user;
 // ********************** Create Application **********************
 const app = (0, express_1.default)();
 // ********************** Middleware And Routes **********************
-// Middleware
-app.use(cors());
-app.use(express_1.default.json());
-// ROUTES
-app.use(UrlConstants_1.URL_CONSTANTS.user, usersRoute);
-// ********************** INIT FUNCTION **********************
-const init = () => __awaiter(void 0, void 0, void 0, function* () {
-    const db = new DatabaseConnection_1.Database();
-    yield db.connectToDb();
-});
-// ********************** INITIALIZE APPLICATION **********************
-init();
+//     // Middleware
+//     app.use(cors())
+//     app.use(express.json())
+//     // ROUTES
+//     app.use(URL_CONSTANTS.user,usersRoute)
+// // ********************** INIT FUNCTION **********************
+// const init = async() => {
+//     const db = new Database();
+//     await db.connectToDb() 
+// }
+// // ********************** INITIALIZE APPLICATION **********************
+// init();
 // ********************** SET PORT **********************
 const PORT = process.env.PORT || 3000 || 8080;
 app.listen(PORT, () => console.log("Server running"));
